52f633ef707d6ccfd13da3c30bc032ace41e1b70
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-09-12 18:29:22 +0530

    done for day

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './index/footer/footer.component';
+import { JobsearchComponent } from './jobsearch/jobsearch.component';
+import { LoginComponent } from './index/login/login.component';
+import { AboutusComponent } from './index/aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: IndexComponent},
+  {path:"job", component: JobsearchComponent},
+  {path:"login", component: LoginComponent},
+  {path:"aboutus", component: AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './index/header/header.component';
+import { FormsModule }   from '@angular/forms';
+import {CarouselModule} from 'primeng/carousel';
+import { FooterComponent } from './index/footer/footer.component';
+import { JobsearchComponent } from './jobsearch/jobsearch.component';
+import { FeaturedjobsComponent } from './index/featuredjobs/featuredjobs.component';
+import { SearchjobComponent } from './jobsearch/searchjob/searchjob.component';
+import { LoginComponent } from './index/login/login.component';
+import { AboutusComponent } from './index/aboutus/aboutus.component';
+import { Header2Component } from './index/header2/header2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    JobsearchComponent,
+    FeaturedjobsComponent,
+    SearchjobComponent,
+    LoginComponent,
+    AboutusComponent,
+    Header2Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
